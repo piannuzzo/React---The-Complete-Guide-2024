80e2164b12a6843bedbe307a1f575a36838e6f48
Peter Iannuzzo <piannuzzo@users.noreply.github.com>
2024-04-01 15:07:58 -0400

    Set focus and select input text on edit

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -2,12 +2,14 @@ import { useState } from "react"
 
 export default function Player({ initialName, symbol, isActive, onChangeName }) {
 	const [ playerName, setPlayerName ] = useState(initialName)
-	const [ isEditing, setIsEditing ] = useState(false)
-
+	const [isEditing, setIsEditing] = useState(false)
+	
 	function handleEditButtonClick() {
 		setIsEditing(editing => !editing)
 		isEditing && onChangeName(symbol, playerName)
 	}
+
+	function handleFocus(event) { event.target.select() }
 
 	function handlePlayerNameChange(event) {
 		setPlayerName(event.target.value)
@@ -18,7 +20,9 @@ export default function Player({ initialName, symbol, isActive, onChangeName }) 
 			<span className="player">
 				{ isEditing
 					? <input
+						autoFocus
 						onChange={handlePlayerNameChange}
+						onFocus={handleFocus}
 						required
 						type="text"
 						value={playerName}
